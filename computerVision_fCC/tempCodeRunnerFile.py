eb60978ba27f93a6fd6cb5dddcd387c63f1543be
David D. <daizpaz19@gmail.com>
2025-12-27 15:42:10 -0600

    corrección para programas de vc

--- computerVision_fCC/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-blur = cv.GaussianBlur(img, (15,1), cv.BORDER_DEFAULT) # matriz en impares a fuerzas
-blur2 = cv.GaussianBlur(img, (1,15), cv.BORDER_DEFAULT) # matriz en impares a fuerzas
-cv.imshow('blur_x', blur)
-cv.imshow('blur_y', blur2)
